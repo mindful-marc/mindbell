d1593cf304ad3b592c29cbf4614473e1a756ec68
marcschroeder108 <marcschroeder108@53c64256-f7b6-11de-9a6a-9d1f184cc70b>
2010-01-02 16:39:45 +0000

    Changed package name to match repository: com.googlecode.mindbell

--- trunk/src/com/googlecode/mindbell/MindBellSettings.java
+++ trunk/src/com/googlecode/mindbell/MindBellSettings.java
@@ -13,11 +13,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.mindbell;
+package com.googlecode.mindbell;
 
 
 
 import java.util.Calendar;
+
+import com.googlecode.mindbell.R;
 
 import android.app.Activity;
 import android.app.AlarmManager;
