0f974df21b930bf94b7bfb77712afd0a487e8eb3
marcschroeder108 <mindful-marc@users.noreply.github.com>
2010-01-02 16:39:45 +0000

    Changed package name to match repository: com.googlecode.mindbell

--- src/com/googlecode/mindbell/MindBell.java
+++ src/com/googlecode/mindbell/MindBell.java
@@ -13,7 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.mindbell;
+package com.googlecode.mindbell;
+
+import com.googlecode.mindbell.R;
 
 import android.app.Activity;
 import android.media.MediaPlayer;
--- src/com/googlecode/mindbell/MindBellScheduler.java
+++ src/com/googlecode/mindbell/MindBellScheduler.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.mindbell;
+package com.googlecode.mindbell;
 
 import java.util.Calendar;
 
