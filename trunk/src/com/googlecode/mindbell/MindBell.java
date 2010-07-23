99ad78098e02764375db0573c3a4d3cd4647768d
marcschroeder108 <marcschroeder108@53c64256-f7b6-11de-9a6a-9d1f184cc70b>
2010-07-23 22:00:25 +0000

    added support for issue 9 (volume control) and issue 12 (mute with phone).
    Dummy preference entries for display issues (issue 7 and issue 10)

--- trunk/src/com/googlecode/mindbell/MindBell.java
+++ trunk/src/com/googlecode/mindbell/MindBell.java
@@ -20,6 +20,7 @@ import com.googlecode.mindbell.R;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -28,6 +29,9 @@ import android.util.Log;
 import android.widget.Toast;
 
 public class MindBell extends Activity {
+	private static MediaPlayer mediaPlayer = null;
+	private static int originalVolume = -1;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,41 +41,83 @@ public class MindBell extends Activity {
     
     protected void onStart() {
     	super.onStart();
-    	
-    	SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
-    	boolean play = true;
-    	// If settings ask us to mute if the phone is active in a call, and that is the case, do not play.
-    	boolean muteOffHook = settings.getBoolean(getString(R.string.keyMuteOffHook), false);
-    	if (muteOffHook) {
-    		TelephonyManager telephonyManager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
-    		if (telephonyManager.getCallState() != TelephonyManager.CALL_STATE_IDLE) {
-    			play = false;
-        		Log.d(MindBellPreferences.LOGTAG, "muting because the phone is off hook");
-        		Toast.makeText(this, "muting because the phone is off hook", Toast.LENGTH_SHORT).show();
-    		}
-    	}
-    	if (play) {
-    		ringBell(this, new MediaPlayer.OnCompletionListener() {
-    			public void onCompletion(MediaPlayer mp) {
-    				mp.release();
-    				MindBell.this.finish();
-    			}
-            });
-    	} else {
-    		finish();
-    	}
+		ringBell(this, new Runnable() {
+			public void run() {
+				MindBell.this.finish();
+			}
+		});
     }
     
     /**
      * Trigger the bell's sound. This is the preferred way to play the sound.
      * @param context the context in which to play the sound.
-     * @param listener an optional listener to call on completion of the sound, or null.
+     * @param runWhenDone an optional Runnable to call on completion of the sound, or null.
      */
-    public static void ringBell(Context context, MediaPlayer.OnCompletionListener listener) {
-    	MediaPlayer mp = MediaPlayer.create(context, R.raw.bell10s);
-    	if (listener != null) {
-    		mp.setOnCompletionListener(listener);
+    public static void ringBell(Context context, final Runnable runWhenDone) {
+    	final AudioManager audioMan = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+    	SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
+    	
+    	// Stop any running bell sounds
+    	if (mediaPlayer != null) {
+    		if (mediaPlayer.isPlaying()) {
+    			mediaPlayer.stop();
+    		}
+			mediaPlayer.release();
+			mediaPlayer = null;
+			// but leave originalVolume as it is
+    	} else {
+    		// no current sound: remember volume
+    		originalVolume = audioMan.getStreamVolume(AudioManager.STREAM_MUSIC);
     	}
-    	mp.start();
+    	
+    	boolean play = true;
+    	// If we are to be muted, don't go any further:
+    	boolean muteWithPhone = settings.getBoolean(context.getString(R.string.keyMuteWithPhone), false);
+    	if (muteWithPhone && audioMan.getStreamVolume(AudioManager.STREAM_RING) == 0) {
+    		play = false;
+			String message = "muting bell because the phone is muted";
+    		Log.d(MindBellPreferences.LOGTAG, message);
+    		Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    	}
+    	
+    	// If settings ask us to mute if the phone is active in a call, and that is the case, do not play.
+    	boolean muteOffHook = settings.getBoolean(context.getString(R.string.keyMuteOffHook), false);
+    	if (muteOffHook) {
+    		TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(TELEPHONY_SERVICE);
+    		if (telephonyManager.getCallState() != TelephonyManager.CALL_STATE_IDLE) {
+    			play = false;
+    			String message = "muting bell because the phone is off hook";
+        		Log.d(MindBellPreferences.LOGTAG, message);
+        		Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    		}
+    	}
+    	if (!play) {
+    		if (runWhenDone != null) {
+    			runWhenDone.run();
+    		}
+			return;
+    	}
+
+    	// OK, let's play the sound
+    	mediaPlayer = MediaPlayer.create(context, R.raw.bell10s);
+    	mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+			public void onCompletion(MediaPlayer mp) {
+				mediaPlayer.release();
+				mediaPlayer = null;
+				if (originalVolume != -1) {
+					audioMan.setStreamVolume(AudioManager.STREAM_MUSIC, originalVolume, 0);
+					originalVolume = -1;
+				}
+				if (runWhenDone != null) {
+					runWhenDone.run();
+				}
+			}
+		});
+    	String bellVolumeString = settings.getString(context.getString(R.string.keyVolume), null);
+    	if (bellVolumeString != null) {
+        	int bellVolume = Integer.valueOf(bellVolumeString);
+        	audioMan.setStreamVolume(AudioManager.STREAM_MUSIC, bellVolume, 0);
+    	}
+    	mediaPlayer.start();
     }
 }
