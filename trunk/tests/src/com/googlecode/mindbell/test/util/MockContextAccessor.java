b731bf2f9d1b1362d21d738c4bcdde4aca6e62cc
marcschroeder108 <marcschroeder108@53c64256-f7b6-11de-9a6a-9d1f184cc70b>
2010-12-26 14:40:24 +0000

    cleanup

--- trunk/tests/src/com/googlecode/mindbell/test/util/MockContextAccessor.java
+++ trunk/tests/src/com/googlecode/mindbell/test/util/MockContextAccessor.java
@@ -1,53 +1,67 @@
+/*
+ * Copyright (C) 2010 Marc Schroeder.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.googlecode.mindbell.test.util;
 
 import com.googlecode.mindbell.util.ContextAccessor;
 
 public class MockContextAccessor extends ContextAccessor {
-	private boolean isSettingMuteWithPhone = false;
-	private boolean isSettingMuteOffHook = false;
-	private boolean isPhoneMuted = false;
-	private boolean isPhoneOffHook = false;
-	
-	public void setSettingMuteWithPhone(boolean value) {
-		isSettingMuteWithPhone = value;
-	}
+    private boolean isSettingMuteWithPhone = false;
+    private boolean isSettingMuteOffHook   = false;
+    private boolean isPhoneMuted           = false;
+    private boolean isPhoneOffHook         = false;
 
-	public void setSettingMuteOffHook(boolean value) {
-		isSettingMuteOffHook = value;
-	}
-	
-	public void setPhoneMuted(boolean value) {
-		isPhoneMuted = value;
-	}
+    @Override
+    public boolean isPhoneMuted() {
+        return isPhoneMuted;
+    }
 
-	public void setPhoneOffHook(boolean value) {
-		isPhoneOffHook = value;
-	}
+    @Override
+    public boolean isPhoneOffHook() {
+        return isPhoneOffHook;
+    }
 
+    @Override
+    public boolean isSettingMuteOffHook() {
+        return isSettingMuteOffHook;
+    }
 
-	@Override
-	public boolean isPhoneMuted() {
-		return isPhoneMuted;
-	}
+    @Override
+    public boolean isSettingMuteWithPhone() {
+        return isSettingMuteWithPhone;
+    }
 
-	@Override
-	public boolean isPhoneOffHook() {
-		return isPhoneOffHook;
-	}
+    public void setPhoneMuted(boolean value) {
+        isPhoneMuted = value;
+    }
 
-	@Override
-	public boolean isSettingMuteWithPhone() {
-		return isSettingMuteWithPhone;
-	}
+    public void setPhoneOffHook(boolean value) {
+        isPhoneOffHook = value;
+    }
 
-	@Override
-	public boolean isSettingMuteOffHook() {
-		return isSettingMuteOffHook;
-	}
+    public void setSettingMuteOffHook(boolean value) {
+        isSettingMuteOffHook = value;
+    }
 
-	@Override
-	public void showMessage(String message) {
-		System.out.println(message);
-	}
+    public void setSettingMuteWithPhone(boolean value) {
+        isSettingMuteWithPhone = value;
+    }
+
+    @Override
+    public void showMessage(String message) {
+        System.out.println(message);
+    }
 
 }
