c98ea41677c003b7c49b6efb6770b4e051ec3d3f
marcschroeder108 <mindful-marc@users.noreply.github.com>
2010-12-28 12:11:38 +0000

    Refactoring ringBell more, so it can be fully run using mock context accessor

--- mindbell-app/src/main/java/com/googlecode/mindbell/MindBell.java
+++ mindbell-app/src/main/java/com/googlecode/mindbell/MindBell.java
@@ -17,11 +17,7 @@ package com.googlecode.mindbell;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.SharedPreferences;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.util.Log;
 
 import com.googlecode.mindbell.util.AndroidContextAccessor;
@@ -29,7 +25,6 @@ import com.googlecode.mindbell.util.ContextAccessor;
 
 public class MindBell extends Activity {
 
-    private static MediaPlayer  mediaPlayer    = null;
     private static int          originalVolume = -1;
     private static final Object lock           = new Object();
 
@@ -47,28 +42,32 @@ public class MindBell extends Activity {
      *            null.
      */
     public static void ringBell(Context context, final Runnable runWhenDone) {
+        ContextAccessor ca = new AndroidContextAccessor(context);
+    }
+
+    /**
+     * Trigger the bell's sound. This is the preferred way to play the sound.
+     * 
+     * @param context
+     *            the context in which to play the sound.
+     * @param runWhenDone
+     *            an optional Runnable to call on completion of the sound, or
+     *            null.
+     */
+    public static void ringBell(ContextAccessor ca, final Runnable runWhenDone) {
         logDebug("Ring bell request received");
-        final AudioManager audioMan = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
 
         synchronized (lock) {
             // Stop any running bell sounds
-            if (mediaPlayer != null) {
-                if (mediaPlayer.isPlaying()) {
-                    mediaPlayer.stop();
-                    logDebug("Stopped ongoing player.");
-                }
-                mediaPlayer.release();
-                mediaPlayer = null;
-                // but leave originalVolume as it is
+            if (ca.haveMediaPlayer()) {
+                ca.destroyMediaPlayer();
             } else {
                 // no current sound: remember volume
-                originalVolume = audioMan.getStreamVolume(AudioManager.STREAM_MUSIC);
+                originalVolume = ca.getOriginalVolume();
                 logDebug("Remembering original music volume: " + originalVolume);
             }
         }
 
-        ContextAccessor ca = new AndroidContextAccessor(context);
         if (ca.isMuteRequested()) {
             if (runWhenDone != null) {
                 runWhenDone.run();
@@ -76,33 +75,7 @@ public class MindBell extends Activity {
             return;
         }
 
-        // OK, let's play the sound
-        synchronized (lock) {
-            mediaPlayer = MediaPlayer.create(context, R.raw.bell10s);
-            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                public void onCompletion(MediaPlayer mp) {
-                    logDebug("Upon completion, originalVolume is " + originalVolume);
-                    synchronized (lock) {
-                        mediaPlayer.release();
-                        if (originalVolume != -1) {
-                            audioMan.setStreamVolume(AudioManager.STREAM_MUSIC, originalVolume, 0);
-                            originalVolume = -1;
-                        }
-                        mediaPlayer = null;
-                    }
-                    if (runWhenDone != null) {
-                        runWhenDone.run();
-                    }
-                }
-            });
-            String bellVolumeString = settings.getString(context.getString(R.string.keyVolume), null);
-            if (bellVolumeString != null) {
-                int bellVolume = Integer.valueOf(bellVolumeString);
-                audioMan.setStreamVolume(AudioManager.STREAM_MUSIC, bellVolume, 0);
-            }
-            logDebug("Ringing bell with volume " + bellVolumeString);
-            mediaPlayer.start();
-        }
+        ca.kickoffMediaPlayer(runWhenDone, originalVolume);
     }
 
     /** Called when the activity is first created. */
